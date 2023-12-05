7ef5a7e5753efbf8257ec339abf116d7d4165505
luispe <luispedrotoloy@gmail.com>
2023-12-04 21:49:07 -0300

    rm premature abstractions, minify code

--- telemetry/trace_test.go
+++ telemetry/trace_test.go
@@ -6,8 +6,6 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/otel/attribute"
 
 	"github.com/pomelo-la/go-toolkit/telemetry"
 )
@@ -104,64 +102,5 @@ func TestConfigShutdownTimeout(t *testing.T) {
 			err = client.ShutdownTraceProvider(tt.args.ctx, telemetry.WithTraceShutdown(tt.args.timeout))
 			assert.Equal(t, tt.want.err, err)
 		})
-	}
-}
-
-func TestAddSpan(t *testing.T) {
-	ctx := context.Background()
-	trace, err := telemetry.NewTrace(ctx, "my-service-name")
-	assert.NoError(t, err)
-
-	_, span := trace.AddSpan(ctx, "go-toolkit.telemetry", attribute.String("key1", "value1"))
-	if span == nil {
-		t.Error("Expected a valid span, but got nil")
-	}
-}
-
-func TestSanitizeMetricTagValue(t *testing.T) {
-	tt := []struct {
-		tag  string
-		want string
-	}{
-		{
-			tag:  "",
-			want: "",
-		},
-		{
-			tag:  "/fixed/{first_param}",
-			want: "/fixed/_first_param",
-		},
-		{
-			tag:  "/{first_param}/{second_param}/fixed",
-			want: "/_first_param/_second_param/fixed",
-		},
-		{
-			tag:  "/fixed/fixed",
-			want: "/fixed/fixed",
-		},
-		{
-			tag:  "/{first_param:[a-z0-9]+}/fixed",
-			want: "/_first_param:[a-z0-9]+/fixed",
-		},
-		{
-			tag:  "/fixed/fixed/",
-			want: "/fixed/fixed",
-		},
-	}
-
-	for _, tc := range tt {
-		t.Run(tc.tag, func(t *testing.T) {
-			name := telemetry.SanitizeMetricTagValue(tc.tag)
-			require.Equal(t, tc.want, name)
-		})
-	}
-}
-
-func TestSanitizeMetricTagValue_MultipleTimes(t *testing.T) {
-	input := telemetry.SanitizeMetricTagValue("/{first_param}/{second_param}/fixed")
-	for i := 0; i < 10; i++ {
-		tmp := telemetry.SanitizeMetricTagValue(input)
-		require.Equal(t, input, tmp)
-		input = tmp
 	}
 }
